c058ca584bebb34039185cccf54cfce047801d8f
rjuchnicki <robert.juchnicki@gmail.com>
2016-12-15 23:56:00 -0500

    Day 12 solution

---
+++ day12/aoc12-both-parts.py
@@ -0,0 +1,28 @@
+f = open('12_input.txt', 'r')
+instructions = [x.strip('\n').split(' ') for x in f.readlines()]
+f.close()
+
+def lookup(v):
+    try:
+        return int(v)
+    except:
+        return registers[v]
+
+i = 0
+registers = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+# registers = {'a': 0, 'b': 0, 'c': 1, 'd': 0} Input for part 2
+while i < len(instructions):
+    ins = instructions[i]
+    if (ins[0] == 'inc'):
+        registers[ins[1]] += 1
+    elif (ins[0] == 'dec'):
+        registers[ins[1]] -= 1
+    elif (ins[0] == 'cpy'):
+        registers[ins[2]] = lookup(ins[1])
+
+    if ins[0] == 'jnz' and lookup(ins[1]) != 0:
+        i = i + int(ins[2])
+    else:
+        i += 1
+
+print registers['a']
