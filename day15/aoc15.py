f64b64376ff89d01abe8cafb1f4cd15c3e212d6f
rjuchnicki <robert.juchnicki@gmail.com>
2016-12-30 11:56:40 -0500

    Day 15 Solution

--- day15/aoc15.py
+++ day15/aoc15.py
@@ -6,14 +6,12 @@ def pass_through(discs):
     for i in xrange(len(discs)):
         if (discs[i][0] + i + 1) % discs[i][1] != 0:
             return False
-
     return True
 
 t = 0
 while not pass_through(discs):
     for pos in discs:
         pos[0] = (pos[0] + 1) % pos[1]
-
     t += 1
 
 print t
